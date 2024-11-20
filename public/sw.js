b1ab9417631a5bdb19fbe74d77179d34e212ad13
DaveEdge1 <david.edge@nau.edu>
2024-11-20 17:04:21 +0000

    again

--- public/sw.js
+++ public/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-13e083c09806591cc0a8.js"
+    "url": "webpack-runtime-fb097e47b18784235f33.js"
   },
   {
     "url": "styles.83fa337ebac205e5d05d.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "94d9842a75fc8897fdd102caccc0f487"
+    "revision": "af102f48d4e5fedc3097f6ff83595b6f"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
